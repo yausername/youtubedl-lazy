bb226cafb83708df2efca474c5d6f15689f5516a
Ritvik Saraf <13ritvik@gmail.com>
2019-02-23 20:17:16 +0530

    add youtube-dl to path

--- create-github-release.py
+++ create-github-release.py
@@ -11,7 +11,7 @@ import os
 import re
 import sys
 
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)),'youtube-dl'))
 
 from youtube_dl.compat import (
     compat_basestring,
