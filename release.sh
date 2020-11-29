2fbbac6fe2335ca480355a2d0ee9c3eabcd53621
yausername <yauser@protonmail.com>
2020-11-29 20:01:58 +0530

    Revert "changed submodule remote"
    This reverts commit 4682707acea484ee6091118351fbd05b6ee34f84.

--- release.sh
+++ release.sh
@@ -44,8 +44,7 @@ esac
 done
 
 #if [ -z "$1" ]; then echo "ERROR: specify version number like this: $0 1994.09.06"; exit 1; fi
-#version="$(curl -s https://ytdl-org.github.io/youtube-dl/update/LATEST_VERSION)"
-version=`git --git-dir=youtube-dl/.git describe --tags --abbrev=0`
+version="$(curl -s https://ytdl-org.github.io/youtube-dl/update/LATEST_VERSION)"
 major_version=$(echo "$version" | sed -n 's#^\([0-9]*\.[0-9]*\.[0-9]*\).*#\1#p')
 #if test "$major_version" '!=' "$(date '+%Y.%m.%d')"; then
 #    echo "$version does not start with today's date!"
